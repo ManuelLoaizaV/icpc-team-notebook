b45e582c066ba04c551622d6864cf5f66ce6aea5
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2020-05-28 00:10:47 -0500

    Long, Double updated

--- other/template_debug.cpp
+++ other/template_debug.cpp
@@ -1,7 +1,6 @@
 // Gracias, Rodolfo
-// Gracias, Roman
-// Gracias, Maxi
 // Gracias, MarcosK
+// Gracias, Graphter
 // Obrigado, Dilson
 //#pragma GCC optimize ("Ofast,unroll-loops")
 //#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
@@ -9,14 +8,13 @@
 #include <unistd.h>
 using namespace std;
 #define fastio ios::sync_with_stdio(0);cin.tie(0)
-#define For(i,a,b) for (int i = a; i < b; i++)
-#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define For(i,a,b) for (Long i = a; i < b; i++)
+#define roF(i,a,b) for (Long i = a; i >= b; i--)
 #define pb push_back
 #define mp make_pair
 #define ff first
 #define ss second
 #define all(v) (v).begin(),(v).end()
-#define SZ(v) ((int)v.size())
 
 void __print(int x) {cerr << x;}
 void __print(long x) {cerr << x;}
@@ -45,19 +43,19 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #define debug(x...)
 #endif
 
-typedef long long ll;
-typedef long double ld;
-typedef unsigned long long ull;
-typedef pair<ll, ll> ii;
-typedef vector<ll> vi;
-typedef vector<ii> vii;
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+typedef vector<Long> Vector;
+typedef vector<Pair> PairVector;
 
 const int N = 1e6;
-const ll INF = 1e18;
-const ld EPS = 10e-9;
+const Long INF = 1e18;
+const Double EPS = 10e-9;
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
 
 void solve() {
 	
