ce6d01b5df634788c1f0a288cd259e5c9f347abc
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-06-02 00:32:57 -0500

    Implementacion recursiva

--- data_structures/segment_tree.cpp
+++ data_structures/segment_tree.cpp
@@ -1,31 +1,31 @@
 const int N = 1e5;
-int tree[4 * N];
-
-void build(int a[], int v, int tl, int tr) {
-	if (tl == tr) {
-		tree[v] = a[tl];
-	} else {
-		int tm = (tl + tr) / 2;
-		build(a, 2 * v, tl, tm);
-		build(a, 2 * v + 1, tm + 1, tr);
-		tree[v] = tree[2 * v] + tree[2 * v + 1];
+struct SegmentTree {
+	int tree[4 * N];	
+	void build(vector<int> &a, int id, int tl, int tr) {
+		if (tl == tr) {
+			tree[id] = a[tl];
+		} else {
+			int tm = (tl + tr) / 2;
+			build(a, 2 * id, tl, tm);
+			build(a, 2 * id + 1, tm + 1, tr);
+			tree[id] = tree[2 * id] + tree[2 * id + 1];
+		}
 	}
-}
-
-int sum(int v, int tl, int tr, int l, int r) {
-	if (l > r) return 0;
-	if (l == tl and r == tr) return tree[v];
-	int tm = (tl + tr) / 2;
-	return sum(2 * v, tl, tm, l, min(r, tm)) + sum(2 * v + 1, tm + 1, tr, max(l, tm + 1), r);
-}
-
-void update(int v, int tl, int tr, int pos, int new_val) {
-	if (tl == tr) {
-		tree[v] = new_val;
-	} else {
-		int tm = (tl + tr) / 2;
-		if (pos <= tm) update(2 * v, tl, tm, pos, new_val);
-		else update(2 * v + 1, tm + 1, tr, pos, new_val);
-		tree[v] = tree[2 * v] + tree[2 * v + 1];
+	void update(int pos, int val, int id, int tl, int tr) {
+		if (tl == tr) {
+			tree[pos] = val;
+		} else {
+			int tm = (tl + tr) / 2;
+			if (pos <= tm) update(pos, val, 2 * id, tl, tm);
+			else update(pos, val, 2 * id + 1, tm + 1, tr);
+			tree[id] = tree[2 * id] + tree[2 * id + 1];
+		}
 	}
-}
+	int query(int l, int r, int id, int tl, int tr) {
+		if (l <= tl and tr <= r) return tree[id];
+		int tm = (tl + tr) / 2;
+		if (r <= tm) return query(l, r, 2 * id, tl, tm);
+		if (tm < l) return query(l, r, 2 * id + 1, tm + 1, tr);
+		return query(l, r, 2 * id, tl, tm) + query(l, r, 2 * id + 1, tm + 1, tr);
+	}
+} st;
