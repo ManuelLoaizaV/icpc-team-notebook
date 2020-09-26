b201f318a894363742cdacdfefb45097fa23b1ba
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-09-25 19:31:09 -0500

    Osman añadido

--- other/template_debug.cpp
+++ other/template_debug.cpp
@@ -1,6 +1,6 @@
 // Gracias, Rodolfo
 // Gracias, Graphter
-// Obrigado, Dilson
+// Gracias, Osman
 //#pragma GCC optimize ("Ofast,unroll-loops")
 //#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include <bits/stdc++.h>
