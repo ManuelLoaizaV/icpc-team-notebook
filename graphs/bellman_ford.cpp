b943f66bf7f340ccbc754011da10a0dd201082c3
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-10-06 18:41:10 -0500

    Bellman-Ford añadido.

---
+++ graphs/bellman_ford.cpp
@@ -0,0 +1,22 @@
+const int INF = 1e9;
+const int N = 5e2;
+struct Edge { int u, v, w; };
+vector<Edge> edges;
+int d[N];
+void BellmanFord(int s) {
+  for (int i = 0; i < N; i++) d[i] = INF;
+  d[s] = 0;
+  // Numero de iteraciones que vamos a hacer del algoritmo
+  for (int i = 0; i < n - 1; i++) {
+    for (Edge e : edges) {
+      d[e.v] = min(d[e.v], d[e.u] + e.w);
+    }
+  }
+  // Determinar ciclos negativos
+  for (Edge e : edges) {
+    if (d[e.u] + e.w < d[e.v]) {
+      cout << "Ciclo negativo encontrado." << '\n';
+      return;
+    }
+  }
+}
