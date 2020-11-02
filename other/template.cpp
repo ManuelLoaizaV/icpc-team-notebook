d995fe1d57e5b1576119b18cd43c9ee8e1915fb7
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-11-02 09:25:53 -0500

    Plantilla sin debug actualizada.

--- other/template.cpp
+++ other/template.cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 #include <unistd.h>
 using namespace std;
-#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
 #define debug(x) cout << #x << " = " << x << endl
 #define For(i,a,b) for (int i = a; i < b; i++)
 #define roF(i,a,b) for (int i = a; i >= b; i--)
@@ -12,7 +12,6 @@ using namespace std;
 #define ff first
 #define ss second
 #define all(v) (v).begin(),(v).end()
-#define SZ(v) ((int)v.size())
 
 typedef long long Long;
 typedef long double Double;
@@ -20,13 +19,13 @@ typedef unsigned long long ULong;
 typedef pair<Long, Long> Pair;
 
 const int N = 1e6;
-const ll INF = 1e18;
-const ld EPS = 1e-9;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
 
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
 
-int main() {
-  fastio;
+int main(void) {
+  FAST_IO;
   return 0;
 }
