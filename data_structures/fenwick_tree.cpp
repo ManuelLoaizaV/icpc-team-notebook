48db8861bb84d0b85a0a6061b504de9c1cabc6bc
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-01-01 18:08:56 -0500

    BIT >= 1D, para 2D anadir una dimension y un while mas.

--- data_structures/fenwick_tree.cpp
+++ data_structures/fenwick_tree.cpp
@@ -1,21 +1,18 @@
 const int N = 1e5;
 struct FenwickTree {
-	int tree[N + 5];
-	void build(int n) { for (int i = 0; i <= n; i++) tree[i] = 0; }
-	// Suma delta a todos los elementos en el rango [i, N]
-	void update(int i, int delta) {
-		while (i <= N) {
-			tree[i] += delta;
-			i += (i & -i);
-		}
-	}
-	// Halla la suma acumulada en la posicion i
-	int query(int i) {
-		int ret = 0;
-		while (i > 0) {
-			ret += tree[i];
-			i -= (i & -i);
-		}
-		return ret;
-	}
+  int tree[N + 1];
+  void Update(int i, int delta) {
+    while (i <= N) {
+      tree[i] += delta;
+      i += (i & -i);
+    }
+  }
+  int Query(int i) {
+    int sum = 0;
+    while (i > 0) {
+      sum += tree[i];
+      i -= (i & -i);
+    }
+    return sum;
+  }
 } ft;
