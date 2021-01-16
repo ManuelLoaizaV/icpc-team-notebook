a410de8e8f1ef0d50393c112cb429e84613d9a6a
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-01-15 21:49:49 -0500

    FFT. CLRS. Chapter 30.

--- math/fast_fourier_transform.cpp
+++ math/fast_fourier_transform.cpp
@@ -1,7 +1,7 @@
 typedef complex<Double> Complex;
 typedef vector<Long> Polynomial;
 const Double PI = acos(-1);
-vector<Complex> DFT(vector<Complex>& a, bool is_inverse) {
+vector<Complex> FFT(vector<Complex>& a, bool is_inverse) {
   int n = a.size();
   if (n == 1) return a;
   Double teta = 2 * PI / n;
@@ -13,8 +13,8 @@ vector<Complex> DFT(vector<Complex>& a, bool is_inverse) {
     a_0[i] = a[2 * i];
     a_1[i] = a[2 * i + 1];
   }
-  vector<Complex> y_0 = DFT(a_0, is_inverse);
-  vector<Complex> y_1 = DFT(a_1, is_inverse);
+  vector<Complex> y_0 = FFT(a_0, is_inverse);
+  vector<Complex> y_1 = FFT(a_1, is_inverse);
   vector<Complex> y(n);
   for (int i = 0; i < n / 2; i++) {
     y[i] = y_0[i] + w * y_1[i];
@@ -34,14 +34,14 @@ Polynomial operator * (Polynomial& a, Polynomial& b) {
   vector<Complex> complex_b(2 * len, 0);
   for (int i = 0; i < m; i++) complex_b[i] = b[i];
   // Evaluation in O(n lg(n))
-  vector<Complex> dft_a = DFT(complex_a, false);
-  vector<Complex> dft_b = DFT(complex_b, false);
+  vector<Complex> dft_a = FFT(complex_a, false);
+  vector<Complex> dft_b = FFT(complex_b, false);
   // Pointwise multiplication in O(n)
   vector<Complex> dft_c(2 * len);
   for (int i = 0; i < p; i++) dft_c[i] = dft_a[i] * dft_b[i];
   // Interpolation in O(n lg(n))
-  vector<Complex> inverse = DFT(dft_c, true);
+  vector<Complex> inverse = FFT(dft_c, true);
   Polynomial c(2 * len);
-  for (int i = 0; i < 2 * len; i++) c[i] = round(inverse[i].real()) / p;
+  for (int i = 0; i < 2 * len; i++) c[i] = round(inverse[i].real() / (2 * len));
   return c;
 }
