15a3749375817941b4af53bb58457f762cc4fdd5
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-01-27 13:40:15 -0500

    DSU actualizado

--- data_structures/disjoint_set_union_find.cpp
+++ data_structures/disjoint_set_union_find.cpp
@@ -1,22 +1,24 @@
-const int N = 1e5;
-struct DSU {
-	int parent[N + 1];
-	// Crea un nuevo set que consiste del nuevo elemento v
-	void make_set(int v) { parent[v] = v; }
-	// Retorna el representante del set que contiene al elemento v
-	int find_set(int u) {
-		if (parent[u] == u) return u;
-		parent[u] = find_set(parent[u]);
-		return parent[u];
-	}
-	// Chequea si dos elementos son parte del mismo set o no
-	bool same_set(int u, int v) {
-		return find_set(u) == find_set(v);
-	}
-	// Une los sets en los cuales u y v se encuentran
-	void union_sets(int u, int v) {
-		int p_u = find_set(u);
-		int p_v = find_set(v);
-		if (p_u != p_v) parent[p_v] = p_u;
-	}
+struct DisjointSets {
+  int parent[N];
+  int size[N];
+  void MakeSet(int u) {
+    parent[u] = u;
+    size[u] = 1;
+  }
+  void Build(int n) { for (int i = 0; i < n; i++) MakeSet(i); }
+  int Find(int u) {
+    if (parent[u] == u) return u;
+    return (parent[u] = Find(parent[u]));
+  }
+  void Join(int u, int v) {
+    u = Find(u);
+    v = Find(v);
+    if (u != v) {
+      if (size[u] > size[v]) swap(u, v);
+      parent[u] = v;
+      size[v] += size[u];
+    }
+  }
+  bool SameSet(int u, int v) { return Find(u) == Find(v); }
+  int GetSize(int u) { return size[Find(u)]; }
 } dsu;
