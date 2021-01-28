b18fac44dcbedb38dd4b8325179f3fa68d83d5dc
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-01-27 20:47:04 -0500

    O((V + E)lg E)

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,19 +1,19 @@
 const int INF = 1e9;
 const int N = 1e5;
-vector<pair<int, int>> adj[N];
-vector<int> d[N];
+vector<Pair> adj[N];
+vector<Long> d[N];
 void Dijkstra(int s) {
   for (int i = 0; i < N; i++) d[i] = INF;
   d[s] = 0;
-  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+  priority_queue<Pair, vector<Pair>, greater<Pair>> q;
   q.push({0, s});
   while (!q.empty()) {
-    pair<int, int> path = q.top();
+    Pair path = q.top();
     int u = path.second;
     q.pop();
     if (d[u] != path.first) continue;
-    for (auto e : adj[u]) {
-      int v = e.first, w = e.second;
+    for (Pair e : adj[u]) {
+      int v = e.first; Long w = e.second;
       if (d[u] + w < d[v]) {
         d[v] = d[u] + w;
         q.push({d[v], v});
