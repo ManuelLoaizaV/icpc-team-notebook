1f771830c570b43b8b0bb7efb2c08088b405e828
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-01-29 12:42:38 -0500

    dfs tree actualizado

--- graphs/graph.cpp
+++ graphs/graph.cpp
@@ -2,15 +2,14 @@ const int INF = 1e9;
 const int N = 1e5;
 struct Graph {
   vector<int> adj[N];
-  bool is_used[N];
   int in[N], out[N], timer;  // DFS
   int d[N];  // BFS
   void Clear(int n) {
     for (int i = 0; i < n; i++) {
       adj[i].clear();
-      is_used[i] = false;
       in[i] = out[i] = 0;
     }
+    timer = 0;
   }
   void AddEdge(int u, int v) {
     adj[u].push_back(v);
@@ -33,9 +32,18 @@ struct Graph {
     }
   }
   void DFS(int u) {
-    is_used[u] = true;
     in[u] = ++timer;
-    for (int v : adj[u]) if (!is_used[v]) DFS(v);
+    for (int v : adj[u]) {
+      if (in[v] == 0) {
+        DFS(v);  // tree edge
+      } else if (out[v] == 0) {
+        // back edge
+      } else if (in[u] < in[v]) {
+        // forward edge
+      } else {
+        // cross edge
+      }
+    }
     out[u] = ++timer;
   }
 } g;
