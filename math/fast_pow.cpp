dae6b826880ff66293fe5b2f249b159241612610
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-03 13:29:01 -0500

    Falta testear

--- math/fast_pow.cpp
+++
@@ -1,9 +0,0 @@
-long long fast_pow(long long a, long long b, long long m) {
-	long long ans = 1LL;
-	while (b > 0) {
-		if (b & 1) ans = (ans * a) % m;
-		a = (a * a) % m;
-		b >= 1;
-	}
-	return ans;
-}
