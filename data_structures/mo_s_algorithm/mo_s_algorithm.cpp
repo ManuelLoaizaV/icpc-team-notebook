3a37002c5cdbd889a19812ba6874c78036019b44
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-03 09:12:27 -0500

    TODO: Actualizar

--- data_structures/mo_s_algorithm/mo_s_algorithm.cpp
+++ data_structures/mo_s_algorithm/mo_s_algorithm.cpp
@@ -1,21 +1,21 @@
-const int SZ = 800;
+const int S = 800;
 struct Query {
   int id, l, r;
   Query() {}
   Query(int id, int l, int r) : id(id), l(l), r(r) {}
   bool operator < (const Query& other) const {
-    int block = l / SZ;
-    int other_block = other.l / SZ;
+    int block = l / S;
+    int other_block = other.l / S;
     if (block != other_block) return (block < other_block);
     return (r > other.r);
   }
 };
 struct Mo {
-  void Add(int val, int& ans) { ans += val; }
-  void Delete(int val, int& ans) { ans -= val; }
-  vector<int> Build(const vector<int>& a, vector<Query>& q) {
+  void Add(Long val, Long& ans) { ans += val; }
+  void Delete(Long val, Long& ans) { ans -= val; }
+  vector<Long> Build(const vector<Long>& a, vector<Query>& q) {
     sort(q.begin(), q.end());
-    vector<int> ans(q.size());
+    vector<Long> ans(q.size());
     int l = 0;
     int r = -1;
     int acc = 0;
