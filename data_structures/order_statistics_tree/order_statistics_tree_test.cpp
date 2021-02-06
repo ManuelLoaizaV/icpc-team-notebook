a532d1a716bc8d8bbef00c116a16f35a9cb8daeb
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-06 06:47:45 -0500

    Clase 04/02/2021

--- data_structures/order_statistics_tree/order_statistics_tree_test.cpp
+++ data_structures/order_statistics_tree/order_statistics_tree_test.cpp
@@ -1,27 +1,27 @@
 //https://www.spoj.com/problems/ORDERS/en/
+#include <algorithm>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 #include <iostream>
 #include <vector>
-#define FAST_IO ios::sync_with_stdio(0);cin.tie(nullptr)
-using namespace std;
 using namespace __gnu_pbds;
+using namespace std;
 
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> OST;
 
 void Solve(void) {
   int n;
   cin >> n;
-  vector<int> cnt(n);
-  for (int i = 0; i < n; i++) cin >> cnt[i];
-  OST aux;
-  for (int i = 1; i <= n; i++) aux.insert(i);
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  OST ost;
   vector<int> ans;
-  for (int len = n - 1; len >= 0; len--) {
-    int pos = len - cnt[len];
-    int cur = *aux.find_by_order(len - cnt[len]);
-    ans.push_back(cur);
-    aux.erase(cur);
+  for (int i = 1; i <= n; i++) ost.insert(-i);
+  for (int i = n - 1; i >= 0; i--) {
+    auto it = ost.find_by_order(a[i]);
+    int val = (-1) * (*it);
+    ans.push_back(val);
+    ost.erase(it);
   }
   reverse(ans.begin(), ans.end());
   for (int i = 0; i < n; i++) {
@@ -32,7 +32,8 @@ void Solve(void) {
 }
 
 int main(void) {
-  FAST_IO;
+  ios::sync_with_stdio(0);
+  cin.tie(nullptr);
   int t;
   cin >> t;
   while (t--) Solve();
