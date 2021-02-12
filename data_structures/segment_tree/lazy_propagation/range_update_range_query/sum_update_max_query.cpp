872e82dbb505beb5c6a2000e1eea2828ed7c5271
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-12 06:53:33 -0500

    Clase 11/02/2021

---
+++ data_structures/segment_tree/lazy_propagation/range_update_range_query/sum_update_max_query.cpp
@@ -0,0 +1,50 @@
+struct SegmentTree {
+  int tree[4 * N], lazy[4 * N];
+  int Merge(const int& x, const int& y) { return max(x, y); }
+  void Build(const vector<int>& a, int id, int tl, int tr) {
+    // Reiniciar
+    lazy[id] = 0;
+    if (tl == tr) {
+      tree[id] = a[tl];
+    } else {
+      int tm = (tl + tr) / 2;
+      Build(a, 2 * id, tl, tm);
+      Build(a, 2 * id + 1, tm + 1, tr);
+      tree[id] = Merge(tree[2 * id], tree[2 * id + 1]);
+    }
+  }
+  void Push(int id, int  tl, int tr) {
+    if (lazy[id] == 0) return;
+    // Aplicar
+    tree[2 * id] += lazy[id];
+    tree[2 * id + 1] += lazy[id];
+    // Acumular
+    lazy[2 * id] += lazy[id];
+    lazy[2 * id + 1] += lazy[id];
+    // Reiniciar
+    lazy[id] = 0;
+  }
+  void Update(int l, int r, int delta, int id, int tl, int tr) {
+    if (tr < l || tl > r) return;
+    if (l <= tl && tr <= r) {
+      // Aplicar
+      tree[id] += delta;
+      // Acumular
+      lazy[id] += delta;
+    } else {
+      int tm = (tl + tr) / 2;
+      Push(id);
+      Update(l, r, delta, 2 * id, tl, tm);
+      Update(l, r, delta, 2 * id + 1, tm + 1, tr);
+      tree[id] = Merge(tree[2 * id], tree[2 * id + 1]);
+    }
+  }
+  int Query(int l, int r, int id, int tl, int tr) {
+    if (l <= tl && tr <= r) return tree[id];
+    int tm = (tl + tr) / 2;
+    Push(id);
+    if (r <= tm) return Query(l, r, 2 * id, tl, tm);
+    if (tm < l) return Query(l, r, 2 * id + 1, tm + 1, tr);
+    return Merge(Query(l, r, 2 * id, tl, tm), Query(l, r, 2 * id + 1, tm + 1, tr));
+  }
+} st;
