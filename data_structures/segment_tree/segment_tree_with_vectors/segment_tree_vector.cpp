ac3d167f457cca42ef84915a25a0a84c07a11fb1
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-15 19:03:54 -0500

    Identado corregido y comentarios anadidos

--- data_structures/segment_tree/segment_tree_with_vectors/segment_tree_vector.cpp
+++ data_structures/segment_tree/segment_tree_with_vectors/segment_tree_vector.cpp
@@ -1,71 +1,63 @@
-const int N = 1e5, INF = 1e9;
+const int N = 1e5;
 struct SegmentTree {
-    vector<int> t[4 * N];//O(nlgn)
-    //O(n)
-    vector<int> combine(vector<int> &L, vector<int> &R) {
-        int pos_L = 0;
-        int pos_R = 0;
-        vector<int> ans;
-        int sz = L.size() + R.size();
-        for (int i = 0; i < sz; i++) {
-            if (pos_L == L.size()) {
-                ans.push_back(R[pos_R]);
-                pos_R++;
-            } else {
-                if (pos_R == R.size()) {
-                    ans.push_back(L[pos_L]);
-                    pos_L++;
-                } else {
-                    if (L[pos_L] < R[pos_R]) {
-                        ans.push_back(L[pos_L]);
-                        pos_L++;
-                    } else {
-                        ans.push_back(R[pos_R]);
-                        pos_R++;
-                    }
-                }
-            }
-        }
-        return ans;
+  vector<int> tree[4 * N];
+  int n;
+  SegmentTree(int new_n) { n = new_n; }
+  vector<int> Merge(const vector<int>& left, vector<int>& right) {
+    int l = 0;
+    int r = 0;
+    vector<int> ans;
+    int size = left.size() + right.size();
+    for (int i = 0; i < size; i++) {
+      if (l == left.size()) {
+        ans.push_back(right[r++]);
+      } else if (r == right.size()) {
+        ans.push_back(left[l++]);
+      } else if (left[l] < right[r]) {
+        ans.push_back(left[l++]);
+      } else {
+        ans.push_back(right[r++]);
+      }
     }
-    int combine(int x, int y) {
-        return min(x, y);
+    return ans;
+  }
+  // O(nlgn)
+  void Build(const vector<int>& a, int id, int tl, int tr) {
+    if (tl == tr) {
+      tree[id] = {a[tl]};
+    } else {
+      int tm = (tl + tr) / 2;
+      Build(a, 2 * id, tl, tm);
+      Build(a, 2 * id + 1, tm + 1, tr);
+      tree[id] = Merge(tree[2 * id], tree[2 * id + 1]);
     }
-    //O(nlgn)
-    void build(vector<int> &a, int id, int tl, int tr) {
-        if (tl == tr) {
-            t[id] = {a[tl]};
-        } else {
-            int tm = (tl + tr) / 2;
-            //int left = id + 1;
-            //int right = id + 2 * (tm - tl + 1);
-            build(a, 2 * id, tl, tm);
-            build(a, 2 * id + 1, tm + 1, tr);
-            t[id] = combine(t[2 * id], t[2 * id + 1]);
-            //merge(t[left].begin(), t[left].end(), t[right].begin(), t[right].end(), back_inserter(t[id]));
-        }
+  }
+  // O(n). Con actualizaciones es mejor utilizar multiset u ost de nodos
+  void Update(int pos, int val, int id, int tl, int tr) {
+    if (tl == tr) {
+      tree[id] = {val};
+    } else {
+      int tm = (tl + tr) / 2;
+      if (pos <= tm) {
+        Update(pos, val, 2 * id, tl, tm);
+      } else {
+        Update(pos, val, 2 * id + 1, tm + 1, tr);
+      }
+      tree[id] = Merge(tree[2 * id], tree[2 * id + 1]);
     }
-    // O(n), si no hay updates, usar vectos basta. Con updates usar multiset u ost
-    void update(int pos, int val, int id, int tl, int tr) {
-        if (tl == tr) {
-            t[id] = {val};
-        } else {
-            int tm = (tl + tr) / 2;
-            if (pos <= tm) update(pos, val, 2 * id, tl, tm);
-            else update(pos, val, 2 * id + 1, tm + 1, tr);
-            t[id] = combine(t[2 * id], t[2 * id + 1]);
-        }
+  }
+  // O(lg^2(n))
+  int Query(int l, int r, int x, int id, int tl, int tr) {
+    if (l <= tl && tr <= r) {
+      auto it = upper_bound(tree[id].begin(), tree[id].end(), x);
+      if (it == tree[id].end()) return 0;
+      int pos = it - tree[id].begin();
+      return tree[id].size() - pos;
     }
-    //O(lg^2(n))
-    int query(int l, int r, int x, int id, int tl, int tr) {
-        if (l <= tl and tr <= r) {
-            auto it = lower_bound(t[id].begin(), t[id].end(), x);
-            if (it == t[id].end()) return INF;
-            else return *it;
-        }
-        int tm = (tl + tr) / 2;
-        if (r <= tm) return query(l, r, x, 2 * id, tl, tm);
-        if (tm < l) return query(l, r, x, 2 * id + 1, tm + 1, tr);
-        return combine(query(l, r, x, 2 * id, tl, tm), query(l, r, x, 2 * id + 1, tm + 1, tr));
-    }
+    int tm = (tl + tr) / 2;
+    if (r <= tm) return Query(l, r, x, 2 * id, tl, tm);
+    if (tm < l) return Query(l, r, x, 2 * id + 1, tm + 1, tr);
+    return Query(l, r, x, 2 * id, tl, tm) + Query(l, r, x, 2 * id + 1, tm + 1, tr);
+  }
+  int Query(int l, int r, int x) { return Query(l, r, x, 1, 0, n - 1); }
 } st;
