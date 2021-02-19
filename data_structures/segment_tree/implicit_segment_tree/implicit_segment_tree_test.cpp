e3af0143fc0d8f83a25469f8e7505426c7c7c6ad
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-19 04:45:08 -0500

    Monkey and apples

--- data_structures/segment_tree/implicit_segment_tree/implicit_segment_tree_test.cpp
+++ data_structures/segment_tree/implicit_segment_tree/implicit_segment_tree_test.cpp
@@ -1,44 +1,57 @@
+//https://www.spoj.com/problems/MON2012/
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
+
 typedef long long Long;
-const Long N = 1e2;
+
+const Long N = 1e9;
 
 struct Node {
-  Long sum, lazy;
+  Long sum;
+  bool lazy;
   Node* left;
   Node* right;
-  Node(void) : sum(0), lazy(0), left(nullptr), right(nullptr) {}
+  Node(void) {
+    sum = 0;
+    lazy = false;
+    left = right = nullptr;
+  }
   void Push(Long tl, Long tr) {
     if (left == nullptr) {
       left = new Node();
       right = new Node();
     }
-    Long tm = (tl + tr) / 2;
-    Long left_size = tm - tl + 1;
-    Long right_size = tr - tm;
-    // Aplicar
-    left->sum += left_size * lazy;
-    right->sum += right_size * lazy;
-    // Propagar
-    left->lazy += lazy;
-    right->lazy += lazy;
-    // Reiniciar
-    lazy = 0;
+    if (lazy) {
+      // Aplicar
+      Long tm = (tl + tr) / 2;
+      left->sum = tm - tl + 1;
+      right->sum = tr - tm;
+      // Propagar
+      left->lazy = true;
+      right->lazy = true;
+      // Reiniciar
+      lazy = false;
+    }
   }
 };
 
 struct SegmentTree {
   Node* root;
-  SegmentTree(void) { root = new Node(); }
-  Long Merge(Long x, Long y) { return x + y; }
+  Long n;
+  SegmentTree(void) {}
+  SegmentTree(Long new_n) {
+    root = new Node();
+    n = new_n;
+  }
+  Long Merge(const Long& x, const Long& y) { return x + y; }
   void Update(Long l, Long r, Long val, Node* node, Long tl, Long tr) {
     if (r < tl || l > tr) return;
+    if (node->lazy) return;
     if (l <= tl && tr <= r) {
       // Aplicar
-      node->sum += (tr - tl + 1) * val;
+      node->sum = (tr - tl + 1) * val;
       // Acumular
-      node->lazy += val;
+      node->lazy = true;
     } else {
       Long tm = (tl + tr) / 2;
       node->Push(tl, tr);
@@ -47,39 +60,37 @@ struct SegmentTree {
       node->sum = Merge(node->left->sum, node->right->sum);
     }
   }
+  void Update(Long l, Long r, Long val) { Update(l, r, val, root, 1, n); }
   Long Query(Long l, Long r, Node* node, Long tl, Long tr) {
     if (l <= tl && tr <= r) return node->sum;
+    if (node->lazy) {
+      return min(r, tr) - max(l, tl) + 1;
+    }
     Long tm = (tl + tr) / 2;
     node->Push(tl, tr);
     if (r <= tm) return Query(l, r, node->left, tl, tm);
     if (tm < l) return Query(l, r, node->right, tm + 1, tr);
     return Merge(Query(l, r, node->left, tl, tm), Query(l, r, node->right, tm + 1, tr));
   }
+  Long Query(Long l, Long r) { return Query(l, r, root, 1, n); }
 };
 
-void Solve(void) {
-  Long n, q;
-  cin >> n >> q;
-  SegmentTree st;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  Long c = 0;
+  int q;
+  scanf("%d", &q);
+  SegmentTree st(N);
   while (q--) {
-    Long t, l, r;
-    cin >> t >> l >> r;
-    l--;
-    r--;
-    if (t == 0) {
-      Long v;
-      cin >> v;
-      st.Update(l, r, v, st.root, 0, N - 1);
+    Long t, x, y;
+    scanf("%lld %lld %lld", &t, &x, &y);
+    if (t == 1) {
+      c = st.Query(x + c, y + c);
+      printf("%lld\n", c);
     } else {
-      cout << st.Query(l, r, st.root, 0, N - 1) << endl;
+      st.Update(x + c, y + c, 1LL);
     }
   }
-}
-
-int main() {
-  fastio;
-  Long t;
-  cin >> t;
-  while (t--) Solve();
   return 0;
 }
