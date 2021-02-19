8f61b3922df607ab4923dab7dffce5169c94b040
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-19 04:37:39 -0500

    Clase 19/02/2021

--- data_structures/segment_tree/implicit_segment_tree/implicit_segment_tree.cpp
+++ data_structures/segment_tree/implicit_segment_tree/implicit_segment_tree.cpp
@@ -1,37 +1,48 @@
+const Long N = 1e9;
 struct Node {
-  Long sum, lazy;
+  Long sum;
+  bool lazy;
   Node* left;
   Node* right;
-  Node(void) : sum(0), lazy(0), left(nullptr), right(nullptr) {}
+  Node(void) {
+    sum = 0;
+    lazy = false;
+    left = right = nullptr;
+  }
   void Push(Long tl, Long tr) {
     if (left == nullptr) {
       left = new Node();
       right = new Node();
     }
-    Long tm = (tl + tr) / 2;
-    Long left_size = tm - tl + 1;
-    Long right_size = tr - tm;
-    // Aplicar
-    left->sum += left_size * lazy;
-    right->sum += right_size * lazy;
-    // Propagar
-    left->lazy += lazy;
-    right->lazy += lazy;
-    // Reiniciar
-    lazy = 0;
+    if (lazy) {
+      // Aplicar
+      Long tm = (tl + tr) / 2;
+      left->sum = tm - tl + 1;
+      right->sum = tr - tm;
+      // Propagar
+      left->lazy = true;
+      right->lazy = true;
+      // Reiniciar
+      lazy = false;
+    }
   }
 };
 struct SegmentTree {
   Node* root;
-  SegmentTree(void) { root = new Node(); }
-  Long Merge(Long x, Long y) { return x + y; }
+  Long n;
+  SegmentTree(void) {}
+  SegmentTree(Long new_n) {
+    root = new Node();
+    n = new_n;
+  }
+  Long Merge(const Long& x, const Long& y) { return x + y; }
   void Update(Long l, Long r, Long val, Node* node, Long tl, Long tr) {
     if (r < tl || l > tr) return;
     if (l <= tl && tr <= r) {
       // Aplicar
-      node->sum += (tr - tl + 1) * val;
+      node->sum = (tr - tl + 1) * val;
       // Acumular
-      node->lazy += val;
+      node->lazy = true;
     } else {
       Long tm = (tl + tr) / 2;
       node->Push(tl, tr);
@@ -40,6 +51,7 @@ struct SegmentTree {
       node->sum = Merge(node->left->sum, node->right->sum);
     }
   }
+  void Update(Long l, Long r, Long val) { Update(l, r, val, root, 0, n - 1); }
   Long Query(Long l, Long r, Node* node, Long tl, Long tr) {
     if (l <= tl && tr <= r) return node->sum;
     Long tm = (tl + tr) / 2;
@@ -48,4 +60,5 @@ struct SegmentTree {
     if (tm < l) return Query(l, r, node->right, tm + 1, tr);
     return Merge(Query(l, r, node->left, tl, tm), Query(l, r, node->right, tm + 1, tr));
   }
-} st;
+  Long Query(Long l, Long r) { return Query(l, r, root, 0, n - 1); }
+};
