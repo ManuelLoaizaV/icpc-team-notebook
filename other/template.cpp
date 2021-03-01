e8aca1b1b7ec472e8a417b6c8d55affafd5e2ea9
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-03-01 00:51:38 -0500

    plantilla actualizada

--- other/template.cpp
+++ other/template.cpp
@@ -2,9 +2,7 @@
 //#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include <bits/stdc++.h>
 #include <unistd.h>
-#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
 #define debug(x) cout << #x << " = " << x << endl
-#define all(v) (v).begin(),(v).end()
 using namespace std;
 
 typedef long long Long;
@@ -20,6 +18,7 @@ mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
 
 int main(void) {
-  FAST_IO;
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   return 0;
 }
---
+++ other/template_contest.cpp
@@ -0,0 +1,151 @@
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> OST;
+// find_by_order(k): Returns an iterator to the k-th largest element (0 indexed).
+// order_of_key(k): Returns the number of items that are stricly smaller than k.
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 69
+#endif
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+const Long MOD = 1e9 + 7;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+Long GetRandom(Long a, Long b) {
+  return uniform_int_distribution<Long> (a, b) (rng);
+}
+
+Long GetBit(Long mask, Long bit) {
+  return (mask >> bit) & 1LL;
+}
+
+void TurnOn(Long& mask, Long bit) {
+  mask = mask | (1LL << bit);
+}
+
+void TurnOff(Long& mask, Long bit) {
+  mask = mask & (~(1 << bit));
+}
+
+Long Add(Long a, Long b) {
+  return (a + b) % MOD;
+}
+
+Long Mul(Long a, Long b) {
+  return (a * b) % MOD;
+}
+
+Long Sub(Long a, Long b) {
+  return (a - b + MOD) % MOD;
+}
+
+void Solve(void) {
+}
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t = 1;
+  //cin >> t;
+  while (t--) Solve();
+  return 0;
+}
