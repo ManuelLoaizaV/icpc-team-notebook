f2d182bd84a083bd579b5a5d1e6dee6ea2729e88
EduardoLlamocaPalomino <eduardo.llamoca@gmail.com>
2021-04-30 20:04:35 -0400

    aristas repetidas

--- graphs/graph_traversal/articulation_points_bridges/articulation_points_bridges.cpp
+++ graphs/graph_traversal/articulation_points_bridges/articulation_points_bridges.cpp
@@ -1,27 +1,30 @@
 const int N = 1e5;
 bool is_art[N + 1];
-int gid = 1, rch = 0, id[N], lo[N];
+int gid = 0, rch = 0, id[N], lo[N];
 vector<int> adj[N];
 vector<pair<int, int>> bridges;
-void clear(int n){
+void clear(int n) {
   bridges.clear();
-  for(int i = 0; i <= n; ++i){
+  for(int i = 0; i <= n; ++i) {
     adj[i].clear();
     id[i] = lo[i] = 0;
     is_art[i] = 0;
   }
 }
 // Resetear rch = 0, gid = 1
-void dfs(int u, int p = -1){
-  id[u] = lo[u] = gid++;
-  for(int v : adj[u]){
-    if (!id[v]) {
+// modifacion con aristas repetidas
+// en vez de p = padre se usa pedge = arista de la que proviene y adj seria vector<pair<int, int>> adj;
+void dfs(int from, int p = -1) {
+  id[from] = lo[from] = ++gid;
+  for(int to : adj[from]) {
+    if (id[to] == 0) {
       if (p == -1) rch++;
-      dfs(v, u);
-      if (lo[v] >= id[u]) is_art[u] = 1;
-      if (lo[v] > id[u]) bridges.pb({u, v});
-      lo[u] = min(lo[u], lo[v]);
-    } else if (v != p) lo[u] = min(lo[u], id[v]);
+      dfs(to, from);
+      lo[from] = min(lo[from], lo[to]);
+      if (lo[to] >= id[from]) is_art[from] = 1;
+      if (lo[to] > id[from]) bridges.pb({from, to});
+    } 
+    else if (to != p) lo[from] = min(lo[from], id[to]);
   }
-  if (p == -1) is_art[u] = (rch > 1);
+  if (p == -1) is_art[from] = (rch > 1);
 }
