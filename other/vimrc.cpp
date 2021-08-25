31e7f18d79d56295ccb9abec60efb04aaa1a20fb
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-08-25 09:48:33 -0500

    Add relative number and ctrl-A

--- other/vimrc.cpp
+++ other/vimrc.cpp
@@ -1,5 +1,5 @@
-// vim ~/.vimrc
 syntax on
+
 :set tabstop=2 softtabstop=2
 :set shiftwidth=2
 :set expandtab
@@ -7,5 +7,14 @@ syntax on
 :set nu
 :set nowrap
 :set incsearch
+:set relativenumber
+
 :set colorcolumn=80
 highlight ColorColumn ctermbg=0 guibg=lightgrey
+
+" Keybinding for ctrl-a to select all
+map <C-a> <esc>ggVG<CR>
+
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
