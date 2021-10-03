11db5c0510482a9b0b3c021856049a8f79a79590
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-10-03 18:31:27 -0500

    Fix bugs

--- code/math/linear-sieve/linear-sieve.cpp
+++ code/math/linear-sieve/linear-sieve.cpp
@@ -2,7 +2,7 @@ vector<Long> f, primes;
 vector<bool> is_prime;
 void Sieve(int n) {
   is_prime = vector<bool>(n + 1, true);
-  f = vector<Long> f(n, 1);
+  f = vector<Long>(n + 1, 1);
   f[1] = 1;
   for (Long i = 2; i <= n; i++) {
     if (is_prime[i]) {
@@ -12,7 +12,7 @@ void Sieve(int n) {
     for (Long j = 0; j < (int) primes.size() && i * primes[j] <= n; j++) {
       is_prime[i * primes[j]] = false;
       if (i % primes[j] == 0) {
-        f[i * primes[j]] = f[i] * prime[j];  // TODO
+        f[i * primes[j]] = f[i] * primes[j];  // TODO
         break;
       } else {
         f[i * primes[j]] = f[i] * f[primes[j]];  // TODO
