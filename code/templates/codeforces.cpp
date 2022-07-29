292b0841d6b47e3a2c0b7b6e0624eb154bd3023c
Manuel Loaiza <manuel.loaiza@pucp.edu.pe>
2022-07-29 07:20:16 -0500

    Update templates

--- code/templates/codeforces.cpp
+++ code/templates/codeforces.cpp
@@ -11,6 +11,7 @@ typedef long long Long;
 typedef long double Double;
 typedef unsigned long long ULong;
 typedef pair<Long, Long> Pair;
+typedef tuple<Long, Long, Long> Trio;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> OST;
 // find_by_order(k): Returns an iterator to the k-th largest element (0 indexed).
 // order_of_key(k): Returns the number of items that are stricly smaller than k.
